961ffcc189a2955d61085c600de6c46282a6d4b7
joon <32847303+suh004757@users.noreply.github.com>
2020-12-13 15:57:04 +0900

    Change to Version2
    English/Korean

---
+++ php_수정중/callmeform-process.php
@@ -0,0 +1,68 @@
+<!-- 수정중 -->
+<?php
+$errorMSG = "";
+
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+if (empty($_POST["phone"])) {
+    $errorMSG = "Phone is required ";
+} else {
+    $phone = $_POST["phone"];
+}
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["select"])) {
+    $errorMSG = "Select is required ";
+} else {
+    $select = $_POST["select"];
+}
+
+if (empty($_POST["terms"])) {
+    $errorMSG = "Terms is required ";
+} else {
+    $terms = $_POST["terms"];
+}
+
+$EmailTo = "yourname@domain.com";
+$Subject = "JBin Project 사이트 문의";
+
+//이메일 본문 텍스트
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Package: ";
+$Body .= $select;
+$Body .= "\n";
+$Body .= "Terms: ";
+$Body .= $terms;
+$Body .= "\n";
+
+// 이메일 보내기
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+// 성공시 
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
---
+++ php_수정중/contactform-process.php
@@ -0,0 +1,60 @@
+<!-- 수정중 -->
+<?php
+$errorMSG = "";
+
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["message"])) {
+    $errorMSG = "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+
+if (empty($_POST["terms"])) {
+    $errorMSG = "Terms is required ";
+} else {
+    $terms = $_POST["terms"];
+}
+
+$EmailTo = "yourname@domain.com";
+$Subject = "JBin Project 사이트 문의";
+
+// 이메일 본문
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+$Body .= "Terms: ";
+$Body .= $terms;
+$Body .= "\n";
+
+// 이메일 보내기
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+
+// 성공시 페이지 
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
